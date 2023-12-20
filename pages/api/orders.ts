e0049cd6c2d342f0d74bd7c3e1088c6241234cd5
bodiabeliy <bboda2106@gmail.com>
2023-12-20 14:21:23 +0200

    configure fake DB

--- pages/api/orders.ts
+++ pages/api/orders.ts
@@ -1,11 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
  
-type ResponseData = {
-  message: string
-}
  
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+export default async function getAllOrders(req: NextApiRequest, res: NextApiResponse) {
+  const response = await fetch(`${process.env._FAKE_DB_CONNECT}/orders`)
   const data = await response.json()
   res.status(200).json(data)
 }
